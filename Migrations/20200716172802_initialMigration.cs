7ff5c6b6269b0ec9faa2dbb1553bdb4983aa8cb9
vishali0 <43428931+vishali0@users.noreply.github.com>
2020-07-17 15:58:34 +0530

    "Added Migration folder"

---
+++ Migrations/20200716172802_initialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Crud_Operation.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Mobile = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customers");
+        }
+    }
+}
